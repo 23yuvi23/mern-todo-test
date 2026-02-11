f142cd0fb4763a8833aa9efee2a63332a4a5cf99
23yuvi23 <yogeshpaliwal9493@gmail.com>
2026-02-10 21:06:19 -0800

    Initial project setup

---
+++ backend/config/database.js
@@ -0,0 +1,16 @@
+// connection to the database
+
+const mongoose = require('mongoose');
+
+const connectDatabase = async () =>{
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Database connected successfully");
+        
+    } catch (error) {
+        console.error('connection failed : ',error.message);
+        process.exit(1)
+    }
+}
+
+module.exports= connectDatabase;
---
+++ backend/testDB.js
@@ -0,0 +1,5 @@
+require("dotenv").config();  // env load karo
+
+const connectDatabase = require("./config/database"); // apni file import karo
+
+connectDatabase();
