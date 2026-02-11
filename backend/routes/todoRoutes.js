574a79be57116b5b6ceb9aab1eaf0586ab61ed1c
23yuvi23 <yogeshpaliwal9493@gmail.com>
2026-02-10 22:04:04 -0800

    models and routes

---
+++ backend/routes/todoRoutes.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const Todo = require('../models/Todo.js')
+
+const router = express.Router();
+
+// get all todos
+router.get('/todos', async (req, res) => {
+    try {
+
+        const todos = Todo.find();
+        res.status(200).json({
+            success: true,
+            todos: todos
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "server error",
+            error: error.message
+        })
+    }
+})
+
+// route 2 add a new todo
+router.post('/todos', async (req, res){
+    try {
+
+        const { text } = req.body
+        console.log("ReceivedTodo: ", text);
+
+        const newTodo = await Todo.create({ text })
+
+        // Success response bhejo
+        res.status(201).json({
+            success: true,
+            message: 'Todo added successfully',
+            todo: newTodo
+        });
+
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "server error",
+            error: error.message
+        })
+    }
+})
+
+module.exports= router
